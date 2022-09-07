7da9af2e8d3f0b4a996717523291ff0568350017
aina gbolahan sunday <aina gbolahan sunday@DESKTOP-167JB72>
2022-09-07 07:42:26 +0100

    Add project files.

---
+++ consoleapp13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd Numbers From 0 to 100");
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+
+
+
+                
+        }
+    }
+}
